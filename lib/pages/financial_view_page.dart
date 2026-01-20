7e7b30ddd6e49e2d162948960c0acc3f858841b8
xPasquale1 <38603532+xPasquale1@users.noreply.github.com>
2026-01-20 14:42:01 +0100

    Financials Übersicht fertig

---
+++ lib/pages/financial_view_page.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/models/financial.dart';
+
+class FinancialViewPage extends StatefulWidget {
+  final Financial financial;
+
+  const FinancialViewPage({super.key, required this.financial});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FinancialViewPageState();
+  }
+}
+
+class _FinancialViewPageState extends State<FinancialViewPage> {
+  late TextEditingController amountController;
+  late TextEditingController descriptionController;
+
+  @override
+  void initState() {
+    amountController = TextEditingController(text: widget.financial.amount.toString());
+    descriptionController = TextEditingController(
+      text: widget.financial.description,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    amountController.dispose();
+    descriptionController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(),
+        title: Text(widget.financial.description, style: TextStyle(fontSize: 32)),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlue,
+        onPressed: () {
+          setState(() {
+            widget.financial.description = descriptionController.text;
+          });
+          Navigator.pop(context);
+        },
+        child: Icon(Icons.save),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Description',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            TextField(controller: descriptionController),
+            Text(
+              'Amount',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            TextField(controller: amountController),
+            Center(
+              child: Padding(
+                padding: EdgeInsets.all(40),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    foregroundColor: Colors.white,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                  },
+                  child: Text(
+                    'Delete Financial',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/financials_page.dart
+++ lib/pages/financials_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:todo_app/components/database.dart';
 import 'package:todo_app/models/financial.dart';
+import 'package:todo_app/pages/financial_view_page.dart';
 import 'package:todo_app/widgets/financial_widget.dart';
 
 class FinancialsPage extends StatefulWidget {
@@ -12,24 +15,122 @@ class FinancialsPage extends StatefulWidget {
 }
 
 class _FinancialsPageState extends State<FinancialsPage> {
-  final List<Financial> financials = [
-    Financial(
-      id: 0,
-      date: DateTime.now(),
-      amount: -10,
-      receiver: 'Test',
-      description: 'Hat gekostet',
-    ),
-    Financial(
-      id: 1,
-      date: DateTime.now(),
-      amount: 4.5,
-      receiver: 'Test 2',
-      description: 'Stonks',
-    ),
-  ];
+  double total = 0;
+  List<Financial> financials = [];
 
-  void onFinancialPressed(Financial financial) {}
+  final euroFormatter = NumberFormat.simpleCurrency(
+    locale: 'de_DE',
+    name: 'EUR',
+    decimalDigits: 2,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    loadFinancials();
+  }
+
+  Future<void> loadFinancials() async {
+    financials = await DB.getAllFinancials();
+    calculateTotal();
+    setState(() {});
+  }
+
+  void calculateTotal() {
+    total = 0;
+    for (Financial financial in financials) {
+      total += financial.amount;
+    }
+  }
+
+  void onFinancialPressed(Financial financial) async {
+    final shouldDelete = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => FinancialViewPage(financial: financial),
+      ),
+    );
+
+    await DB.updateFinancial(financial);
+    calculateTotal();
+    setState(() {});
+
+    if (shouldDelete == null || !shouldDelete) return;
+    await DB.deleteFinancial(financial);
+    setState(() {
+      financials.remove(financial);
+      calculateTotal();
+    });
+  }
+
+  Future<void> addFinancialDialog() async {
+    final descriptionInputController = TextEditingController();
+    final receiverInputController = TextEditingController();
+    final amountInputController = TextEditingController();
+
+    final result = await showDialog<Map<String, String>>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Add new Financial'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  controller: descriptionInputController,
+                  autofocus: true,
+                  decoration: const InputDecoration(hintText: 'Description'),
+                ),
+                TextField(
+                  keyboardType: TextInputType.numberWithOptions(
+                    signed: true,
+                    decimal: true,
+                  ),
+                  controller: amountInputController,
+                  decoration: const InputDecoration(hintText: 'Amount'),
+                ),
+                TextField(
+                  controller: receiverInputController,
+                  decoration: const InputDecoration(hintText: 'Receiver'),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context, {}),
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pop(context, {
+                'description': descriptionInputController.text,
+                'amount': amountInputController.text,
+                'receiver': receiverInputController.text,
+              }),
+              child: const Text('Submit'),
+            ),
+          ],
+        );
+      },
+    );
+    if (result == null) return;
+    if (!result.containsKey('amount')) return;
+    double amount = NumberFormat.decimalPattern(
+      'de_DE',
+    ).parse(result['amount'] as String).toDouble();
+    String description = result.containsKey('description')
+        ? result['description'] as String
+        : '';
+    String receiver = result.containsKey('receiver')
+        ? result['receiver'] as String
+        : '';
+    Financial? financial = await DB.addFinancial(amount, receiver, description);
+    if (financial == null) return;
+    setState(() {
+      financials.add(financial);
+      calculateTotal();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,16 +142,36 @@ class _FinancialsPageState extends State<FinancialsPage> {
         ),
         centerTitle: true,
       ),
-      body: ListView(
-        children: financials.map((financial) {
-          return FinancialWidget(
-            financial: financial,
-            onPress: (financial) => onFinancialPressed(financial),
-          );
-        }).toList(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              total >= 0
+                  ? '+${euroFormatter.format(total)}'
+                  : euroFormatter.format(total),
+              style: TextStyle(
+                color: total >= 0 ? Colors.green : Colors.red,
+                fontWeight: FontWeight.bold,
+                fontSize: 32,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: financials.map((financial) {
+                return FinancialWidget(
+                  financial: financial,
+                  onPress: (financial) => onFinancialPressed(financial),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: addFinancialDialog,
         child: Icon(Icons.add),
       ),
     );
